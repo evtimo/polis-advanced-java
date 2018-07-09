68ae038a605fcc0a6a39653353d95d9563f4e2aa
Evgeny <zheny.tim@gmail.com>
2018-07-09 03:57:00 +0300

    Revert "Dining philosophers problem"
    This reverts commit 54e6256383d647f35e28d132c334465450e94ca0.

--- season2/episode3/src/main/java/edu/technopolis/advancedjava/Deadlock.java
+++ season2/episode3/src/main/java/edu/technopolis/advancedjava/Deadlock.java
@@ -1,62 +1,42 @@
 package edu.technopolis.advancedjava;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Deadlock {
+    private static final Object FIRST_LOCK = new Object();
+    private static final Object SECOND_LOCK = new Object();
 
-    public static final int N = 5; //Кол-во философов = кол-во вилок
-    public static final int SECONDS = 5;   //Время работы программы
-
-
-    public static void main(String[] args) throws InterruptedException {
-        List<Fork> forks = new ArrayList<>();
-        AtomicInteger[] eaten = new AtomicInteger[N];
-        AtomicInteger[] rejected = new AtomicInteger[N];
-        for (int i = 0; i < N; i++) {
-            forks.add(new Fork());
-            eaten[i] = new AtomicInteger(0);
-            rejected[i] = new AtomicInteger(0);
-        }
-
-        List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            Philosopher p = new Philosopher(forks.get(i), forks.get((i + 1) % N), eaten[i], rejected[i]);
-            Thread t = new Thread(p);
-            threads.add(t);
-            t.start();
-        }
-
-        Thread.sleep(1000L * SECONDS);
-
-        threads.forEach(Thread::interrupt);
-
-
-        printResult("Ресурса получено (съедено):", eaten);
-        printResult("Отклонено попыток:", rejected);
+    public static void main(String[] args) throws Exception {
+        Thread ft = new Thread(Deadlock::first);
+        Thread st = new Thread(Deadlock::second);
+        ft.start();
+        st.start();
+        ft.join();
+        st.join();
+        //never going to reach this point
     }
 
-    public static void printResult(String s, AtomicInteger[] a) {
-        int sum = 0;
-        int[] answer = new int[a.length];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = a[i].get();
-            sum += answer[i];
+    private static void first() {
+        synchronized(FIRST_LOCK) {
+            //insert some code here to guarantee a deadlock
+            synchronized(SECOND_LOCK) {
+                //unreachable point
+            }
         }
-        String[] percentage = new String[answer.length];
-        for (int i = 0; i < answer.length; i++) {
-            double percents = 10000.0 * answer[i] / sum;
-            long round = Math.round(percents);
-            percentage[i] = "" + round / 100 + "."
-                    + (round % 100 < 10 ? "0" : "") + round % 100 + "%";
-        }
-
-        System.out.println(s + " " + sum + ": " + Arrays.toString(answer));
-        System.out.println("В процентах: " + Arrays.toString(percentage));
-        System.out.println();
     }
 
+    private static void second() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            //
+        }
+        //reverse order of monitors
+        synchronized(SECOND_LOCK) {
+            //insert some code here to guarantee a deadlock
+            synchronized(FIRST_LOCK) {
+                //unreachable point
+            }
+        }
+
+    }
 
 }
